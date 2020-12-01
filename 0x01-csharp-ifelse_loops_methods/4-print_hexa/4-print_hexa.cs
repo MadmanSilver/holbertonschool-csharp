13b9abaf708e0abf9255b2a04d649e48339c7cc5
Sean Taylor <silverwolf4350@gmail.com>
2020-12-01 16:04:34 -0600

    Changed from WriteLine to Write.

--- 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -5,6 +5,6 @@ class Program
     static void Main(string[] args)
     {
         for (int i = 0; i < 99; i++)
-            Console.WriteLine("{0:D} = 0x{0:X}", i);
+            Console.Write("{0:D} = 0x{0:X}\n", i);
     }
 }
