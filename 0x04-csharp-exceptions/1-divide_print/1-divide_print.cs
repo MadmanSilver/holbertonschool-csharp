232d6dfca18240d208a9c9f99ced727c583fd6a0
Sean Taylor <silverwolf4350@gmail.com>
2020-12-14 12:40:20 -0600

    Created all files for mandatory tasks.

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b) {
+        int res = 0;
+
+        try {
+            res = a / b;
+        } catch {
+            Console.WriteLine("Cannot divide by zero");
+        } finally {
+            Console.WriteLine("{0} / {1} = {2}", a, b, res);
+        }
+    }
+}
