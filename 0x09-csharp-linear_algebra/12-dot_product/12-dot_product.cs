3d5091b50a82269f8ee0f75f60997ab58195b3fd
Sean Taylor <silverwolf4350@gmail.com>
2021-01-19 12:46:51 -0600

    Added dot product calculation.

--- 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 
-namespace _12_dot_product
+/// <summary> Contains functions for vector math. </summary>
+class VectorMath
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+    /// <summary> Calculates the dot product of two vectors. </summary>
+    public static double DotProduct(double[] vector1, double[] vector2) {
+        if (vector1.Length != vector2.Length) {
+            return -1;
         }
+
+        if (vector1.Length == 2) {
+            return vector1[0] * vector2[0] + vector1[1] * vector2[1];
+        }
+
+        if (vector1.Length == 3) {
+            return vector1[0] * vector2[0] + vector1[1] * vector2[1] + vector1[2] * vector2[2];
+        }
+
+        return -1;
     }
 }
