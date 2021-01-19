89af3d596e72b276b8b086a00324c54c10829cdc
Sean Taylor <silverwolf4350@gmail.com>
2021-01-19 12:07:18 -0600

    Vector addition function

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 
-namespace _6_vector_addition
+/// <summary> Contains functions for vector math. </summary>
+class VectorMath
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+    /// <summary> Adds two vectors together. </summary>
+    public static double[] Add(double[] vector1, double[] vector2) {
+        if (vector1.Length != vector2.Length) {
+            return new double[] {-1};
         }
+
+        if (vector1.Length == 2) {
+            return new double[] {vector1[0] + vector2[0], vector1[1] + vector2[2]};
+        }
+
+        if (vector1.Length == 3) {
+            return new double[] {vector1[0] + vector2[0], vector1[1] + vector2[2], vector1[2] + vector2[2]};
+        }
+
+        return new double[] {-1};
     }
 }
