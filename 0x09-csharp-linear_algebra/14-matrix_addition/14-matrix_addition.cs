970929c6e2e14d4aa29266622fb6d51d57496351
Sean Taylor <silverwolf4350@gmail.com>
2021-01-19 16:24:03 -0600

    Added matrix addition.

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 
-namespace _14_matrix_addition
+/// <summary> Contains functions for matrix math. </summary>
+class MatrixMath
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+    /// <summary> Adds two matrices together. </summary>
+    public static double[,] Add(double[,] matrix1, double[,] matrix2) {
+        if (matrix1.Length != matrix2.Length || matrix1.GetLength(0) != matrix2.GetLength(0)) {
+            return new double[,] {{-1}};
         }
+
+        if (matrix1.GetLength(0) == 2 && matrix1.GetLength(1) == 2) {
+            return new double[,] {{matrix1[0,0] + matrix2[0,0], matrix1[0,1] + matrix2[0,1]}, {matrix1[1,0] + matrix2[1,0], matrix1[1,1] + matrix2[1,1]}};
+        }
+
+        if (matrix1.GetLength(0) == 3 && matrix1.GetLength(1) == 3) {
+            return new double[,] {{matrix1[0,0] + matrix2[0,0], matrix1[0,1] + matrix2[0,1], matrix1[0,2] + matrix2[0,2]}, {matrix1[1,0] + matrix2[1,0], matrix1[1,1] + matrix2[1,1], matrix1[1,2] + matrix2[1,2]}, {matrix1[2,0] + matrix2[2,0], matrix1[2,1] + matrix2[2,1], matrix1[2,2] + matrix2[2,2]}};
+        }
+
+        return new double[,] {{-1}};
     }
 }
