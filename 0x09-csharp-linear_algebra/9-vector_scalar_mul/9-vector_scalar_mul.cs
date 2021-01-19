7c67ae56a9f402f9f3545a5d67e15007ccfa6206
Sean Taylor <silverwolf4350@gmail.com>
2021-01-19 12:35:38 -0600

    Added vector by scalar multiplication.

--- 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
-namespace _9_vector_scalar_mul
+/// <summary> Contains functions for vector math. </summary>
+class VectorMath
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+    /// <summary> Multiplies a vector by a scalar. </summary>
+    public static double[] Multiply(double[] vector, double scalar) {
+        if (vector.Length == 2) {
+            return new double[] {vector[0] * scalar, vector[1] * scalar};
         }
+
+        if (vector.Length == 3) {
+            return new double[] {vector[0] * scalar, vector[1] * scalar, vector[2] * scalar};
+        }
+
+        return new double[] {-1};
     }
 }
