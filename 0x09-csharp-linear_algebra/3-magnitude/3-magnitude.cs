01f00f67c3d4e6e9f9f94e0e49129a50d2a35ef3
Sean Taylor <silverwolf4350@gmail.com>
2021-01-19 01:48:42 -0600

    Fixed typo.

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -6,11 +6,11 @@ class VectorMath
     /// <summary> Calculates the magnitude of a vector. </summary>
     public static double Magnitude(double[] vector) {
         if (vector.length == 2) {
-            return Math.sqrt(vector[0] * vector[0] + vector[1] * vector[1]);
+            return Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1]);
         }
 
         if (vector.length == 3) {
-            return Math.sqrt(vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2]);
+            return Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2]);
         }
 
         return -1;
