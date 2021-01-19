76baea4c36f697f8651cd005a84fcf51aa24c98e
Sean Taylor <silverwolf4350@gmail.com>
2021-01-19 11:49:13 -0600

    Rounding output.

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -6,11 +6,11 @@ class VectorMath
     /// <summary> Calculates the magnitude of a vector. </summary>
     public static double Magnitude(double[] vector) {
         if (vector.Length == 2) {
-            return Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1]);
+            return Math.Round(Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1]), 2);
         }
 
         if (vector.Length == 3) {
-            return Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2]);
+            return Math.Round(Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2]), 2);
         }
 
         return -1;
