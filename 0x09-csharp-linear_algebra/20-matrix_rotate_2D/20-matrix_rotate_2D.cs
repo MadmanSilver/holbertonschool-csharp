ac55b1de04d9eb4427cbd217b67ae83387bc4b09
Sean Taylor <silverwolf4350@gmail.com>
2021-01-20 17:58:15 -0600

    Fixed a typo and added rounding.

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -6,7 +6,7 @@ class MatrixMath
     /// <summary> Rotates a 2D matrix by radians. </summary>
     public static double[,] Rotate2D(double[,] matrix, double angle) {
         if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2) {
-            return new double[,] {{matrix[0,0] * Math.Cos(angle) - matrix[0,1] * Math.Sin(angle), matrix[0,0] * Math.Sin(angle) + matrix[0,1] * Math.Cos(angle)}, {matrix[0,0] * Math.Cos(angle) - matrix[0,1] * Math.Sin(angle), matrix[1,0] * Math.Sin(angle) + matrix[1,1] * Math.Cos(angle)}};
+            return new double[,] {{Math.Round(matrix[0,0] * Math.Cos(angle) - matrix[0,1] * Math.Sin(angle), 2), Math.Round(matrix[0,0] * Math.Sin(angle) + matrix[0,1] * Math.Cos(angle), 2)}, {Math.Round(matrix[1,0] * Math.Cos(angle) - matrix[1,1] * Math.Sin(angle), 2), Math.Round(matrix[1,0] * Math.Sin(angle) + matrix[1,1] * Math.Cos(angle), 2)}};
         }
 
         return new double[,] {{-1}};
