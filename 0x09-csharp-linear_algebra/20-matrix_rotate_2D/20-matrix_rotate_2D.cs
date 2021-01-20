2dff72c1f9448e530abc0a25730c674ca4bf4587
Sean Taylor <silverwolf4350@gmail.com>
2021-01-20 17:52:45 -0600

    Added matrix rotation.

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 
-namespace _20_matrix_rotate_2D
+/// <summary> Contains functions for matrix math. </summary>
+class MatrixMath
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+    /// <summary> Rotates a 2D matrix by radians. </summary>
+    public static double[,] Rotate2D(double[,] matrix, double angle) {
+        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2) {
+            return new double[,] {{matrix[0,0] * Math.Cos(angle) - matrix[0,1] * Math.Sin(angle), matrix[0,0] * Math.Sin(angle) + matrix[0,1] * Math.Cos(angle)}, {matrix[0,0] * Math.Cos(angle) - matrix[0,1] * Math.Sin(angle), matrix[1,0] * Math.Sin(angle) + matrix[1,1] * Math.Cos(angle)}};
         }
+
+        return new double[,] {{-1}};
     }
 }
