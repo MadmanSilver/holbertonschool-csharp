77ae11f8544aafae13d5b194a248615a43881b4a
Sean Taylor <silverwolf4350@gmail.com>
2021-01-21 15:36:33 -0600

    Added matrix determinant calculations.

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
-namespace _28_determinant
+/// <summary> Contains functions for matrix math. </summary>
+class MatrixMath
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+    /// <summary> Calculates the determinant of a matrix. </summary>
+    public static double Determinant(double[,] matrix) {
+        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2) {
+            return matrix[0,0] * matrix[1,1] - matrix[0,1] * matrix[1,0];
         }
+
+        if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3) {
+            return matrix[0,0] * (matrix[1,1] * matrix[2,2] - matrix[1,2] * matrix[2,1]);
+        }
+
+        return -1;
     }
 }
