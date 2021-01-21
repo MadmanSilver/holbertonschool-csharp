d31265a0f928f83d231ba17e85593bfed3527ee9
Sean Taylor <silverwolf4350@gmail.com>
2021-01-21 16:12:35 -0600

    Added missing semicolon.

--- 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -9,6 +9,6 @@ class VectorMath
             return new double[] {-1};
         }
 
-        return new double[] {vector1[1] * vector2[2] - vector1[2] * vector2[1], vector1[0] * vector2[2] - vector1[2] * vector2[0], vector1[0] * vector2[1] - vector1[1] * vector2[0]}
+        return new double[] {vector1[1] * vector2[2] - vector1[2] * vector2[1], vector1[0] * vector2[2] - vector1[2] * vector2[0], vector1[0] * vector2[1] - vector1[1] * vector2[0]};
     }
 }
