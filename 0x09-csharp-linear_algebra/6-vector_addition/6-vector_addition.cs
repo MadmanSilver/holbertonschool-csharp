e9803141bc461f98e873be7a3120631ba5a01a98
Sean Taylor <silverwolf4350@gmail.com>
2021-01-21 18:06:56 -0600

    Fixed a typo.

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -14,7 +14,7 @@ class VectorMath
         }
 
         if (vector1.Length == 3) {
-            return new double[] {vector1[0] + vector2[0], vector1[1] + vector2[2], vector1[2] + vector2[2]};
+            return new double[] {vector1[0] + vector2[0], vector1[1] + vector2[1], vector1[2] + vector2[2]};
         }
 
         return new double[] {-1};
