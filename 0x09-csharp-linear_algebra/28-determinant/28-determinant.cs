36e22693f407470e94780b760dc070ef878cde73
Sean Taylor <silverwolf4350@gmail.com>
2021-01-21 18:27:39 -0600

    Fixed typo.

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -10,7 +10,9 @@ class MatrixMath
         }
 
         if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3) {
-            return matrix[0,0] * (matrix[1,1] * matrix[2,2] - matrix[1,2] * matrix[2,1]) - matrix[0,1] * (matrix[1,0] * matrix[2,2] - matrix[1,2] * matrix[2,0]) + matrix[0,2] * (matrix[1,0] * matrix[2,1] - matrix[1,1] * matrix[2,0]);
+            return matrix[0,0] * (matrix[1,1] * matrix[2,2] - matrix[1,2] * matrix[2,1])
+                    - matrix[0,1] * (matrix[1,0] * matrix[1,1] - matrix[1,2] * matrix[2,0])
+                    + matrix[0,2] * (matrix[1,0] * matrix[2,1] - matrix[1,1] * matrix[2,0]);
         }
 
         return -1;
