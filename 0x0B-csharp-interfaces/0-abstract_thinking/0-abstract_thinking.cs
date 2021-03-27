907d1c53098d9966befac4bbba956b5bd85606fc
Sean Taylor <silverwolf4350@gmail.com>
2021-03-27 17:42:28 -0500

    Added method documentation and reverted ToString.

--- 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -4,7 +4,8 @@
 abstract class Base {
     public string name;
 
+    /// <summary> Override of the ToString method. </summary>
     public override string ToString() {
-        return (name + " is a " + this.GetType());
+        return $"{name} is a {this.GetType()}";
     }
 }
